46f7dfa39c2dbae8692ab93437fd7c5d93bc4acb
RaistlinD <Johnd0712@gmail.com>
2018-05-26 22:42:19 +0800

    ok

--- src/cn/krynn/HelloWorld.java
+++ src/cn/krynn/HelloWorld.java
@@ -1,7 +1,8 @@
 package cn.krynn;
 
 //just take a test.
-// java is the NO.1 of programe language.
+//java is the NO.1 of programe language.
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world!");
